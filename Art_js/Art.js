340eb7b9e7e6006d2f1208c1248943c2a157826c
Ismaellucas-BR <ismaellucas36@gmail.com>
2021-11-02 18:35:04 -0300

    Art

---
+++ Art_js/Art.js
@@ -0,0 +1,72 @@
+function passou(){
+    document.getElementById("teste").style.backgroundColor="crimson"
+    document.getElementById("subTitle").style.color="white"
+    document.getElementById("subTitl").style.color="white"
+    document.getElementById("btn").style.color="black"
+    document.getElementById("btn").style.backgroundColor="white"
+    
+}
+
+function saiu(){
+    document.getElementById("teste").style.backgroundColor="white"
+    document.getElementById("subTitle").style.color="black"
+    document.getElementById("subTitl").style.color="black"
+    document.getElementById("btn").style.color="white"
+    document.getElementById("btn").style.backgroundColor="black"
+}
+
+function pass2(){
+    document.getElementById("img-2").style.transform="scale(1.1)"
+    document.getElementById("subTitle2").style.color="crimson"
+    document.getElementById("subTitl2").style.color="SlateGrey"
+}
+function out2(){
+    document.getElementById("img-2").style.transform="scale(1.0)"
+    document.getElementById("subTitle2").style.color="black"
+    document.getElementById("subTitl2").style.color="black"
+
+}
+function pass3(){
+    document.getElementById("img-3").style.transform="scale(1.1)"
+    document.getElementById("subTitle3").style.color="crimson"
+    document.getElementById("subTitl3").style.color="SlateGrey"
+}
+function out3(){
+    document.getElementById("img-3").style.transform="scale(1.0)"
+    document.getElementById("subTitle3").style.color="black"
+    document.getElementById("subTitl3").style.color="black"
+
+}
+function pass4(){
+    document.getElementById("img-4").style.transform="scale(1.1)"
+    document.getElementById("subTitle4").style.color="crimson"
+    document.getElementById("subTitl4").style.color="SlateGrey"
+}
+function out4(){
+    document.getElementById("img-4").style.transform="scale(1.0)"
+    document.getElementById("subTitle4").style.color="black"
+    document.getElementById("subTitl4").style.color="black"
+
+}
+function pass5(){
+    document.getElementById("img-5").style.transform="scale(1.1)"
+    document.getElementById("subTitle5").style.color="crimson"
+    document.getElementById("subTitl5").style.color="SlateGrey"
+}
+function out5(){
+    document.getElementById("img-5").style.transform="scale(1.0)"
+    document.getElementById("subTitle5").style.color="black"
+    document.getElementById("subTitl5").style.color="black"
+
+}
+function pass6(){
+    document.getElementById("img-6").style.transform="scale(1.1)"
+    document.getElementById("subTitle6").style.color="crimson"
+    document.getElementById("subTitl6").style.color="SlateGrey"
+}
+function out6(){
+    document.getElementById("img-6").style.transform="scale(1.0)"
+    document.getElementById("subTitle6").style.color="black"
+    document.getElementById("subTitl6").style.color="black"
+
+}
